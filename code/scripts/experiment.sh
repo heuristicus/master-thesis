e2a5ab5d05f02b69e33c34cf146b897f52f9e3c3
Michal Staniaszek <m.staniaszek@gmail.com>
2015-05-21 23:15:26 +0200

    SHOT explanation

--- code/scripts/experiment.sh
+++ code/scripts/experiment.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 features="shot shotcolor pfh fpfh pfhrgb"
-fselect="iss susan sift"
+fselect="uniform iss susan sift"
 
-process_dirs="/home/michal/Downloads/pcddata/processed/paramtest/iter100"
-
-# /home/michal/Downloads/pcddata/processed/paramtest/ds0_015 /home/michal/Downloads/pcddata/processed/paramtest/dt0,02_mp8000_pp0,001
+process_dirs="/home/michal/Downloads/pcddata/processed/paramtest/ds0_015 /home/michal/Downloads/pcddata/processed/paramtest/dt0,02_mp8000_pp0,001"
 
 # needs attention
 # /home/michal/Downloads/pcddata/processed/paramtest/dt0_02 
 
 # completed 
-# /home/michal/Downloads/pcddata/processed/paramtest/ds0_02 /home/michal/Downloads/pcddata/processed/paramtest/iter500 /home/michal/Downloads/pcddata/processed/paramtest/ds0_02mp_4500 /home/michal/Downloads/pcddata/processed/paramtest/iter50 /home/michal/Downloads/pcddata/processed/paramtest/nr0_04
+# /home/michal/Downloads/pcddata/processed/paramtest/ds0_02 /home/michal/Downloads/pcddata/processed/paramtest/iter500 /home/michal/Downloads/pcddata/processed/paramtest/ds0_02mp_4500 /home/michal/Downloads/pcddata/processed/paramtest/iter50 /home/michal/Downloads/pcddata/processed/paramtest/nr0_04 /home/michal/Downloads/pcddata/processed/paramtest/iter100
 
 for dir in $process_dirs; do
     echo $dir
