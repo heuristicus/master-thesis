f2e22a42bcf35fd4932e550d40e0229d289a871b
Michal Staniaszek <m.staniaszek@gmail.com>
2015-05-27 17:17:58 +0200

    Finished writing object query

--- code/scripts/query/run_queries.sh
+++ code/scripts/query/run_queries.sh
@@ -3,10 +3,10 @@
 querybase="/home/michal/Downloads/pcddata/processed/query/queryobjects/0,01"
 base="/media/michal/Pauli/masterdata/processed/paramtest"
 outbase="/home/michal/Downloads/pcddata/processed/query"
-dirs="dt0,02_mp8000_pp0,001" # iter100 iter50 iter500 nr0_04 #ds0_015 ds0_02mp_4500 
-features="shot shotcolor fpfh pfh pfhrgb"
-fselect="uniform iss susan sift"
-objects="backpack2 chair1 hanger_jacket helmet laptop1 mug1 pillow top_couch_jacket2 trash_bin"
+dirs="iter100" # iter50 iter500 nr0_04 ds0_015 ds0_02mp_4500 #dt0,02_mp8000_pp0,001
+features="shot shotcolor pfh fpfh pfhrgb"
+fselect="uniform sift susan iss"
+objects="backpack2 chair1 hanger_jacket helmet laptop1 pillow top_couch_jacket2" # trash_bin
 
 var=0
 for dir in $dirs; do
@@ -14,8 +14,9 @@ for dir in $dirs; do
     for obj in $objects; do
 	for selection in $fselect; do
     	    for ftype in $features; do
-		qfile=`find $querybase/$obj/features/ -regex .*$ftype\_$selection.*`
+		qfile=`find $querybase/$obj/features/ -regex .*\<$ftype\_$selection.*`
 		matchstr="nonPlanes<${ftype}_$selection"
+		echo "roslaunch object_query object_query.launch query:=$qfile target:=$base/$dir n_max_points:=200 match:=$matchstr cluster_tolerance:=0.2 K:=5 results_out:=$outbase/$dir"
 		roslaunch object_query object_query.launch query:=$qfile target:=$base/$dir n_max_points:=200 match:=$matchstr cluster_tolerance:=0.2 K:=5 results_out:=$outbase/$dir
 		((var++))
 	    done
