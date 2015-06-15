59720e2224e75fa2231d96843664af141e3a802d
Michal Staniaszek <m.staniaszek@gmail.com>
2015-06-15 11:03:19 +0200

    removed and redid

--- search/groups_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['preprocessing',['Preprocessing',['../group__Preprocessing.html',1,'']]]
-];
--- search/pages_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['object_20search_20_2d_20searching_20for_20stuff_20in_20the_20clouds',['Object Search - Searching for stuff in the clouds',['../index.html',1,'']]]
-];
